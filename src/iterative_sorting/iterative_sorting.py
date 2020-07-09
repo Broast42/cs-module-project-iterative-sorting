90f8d802e3112e94d6acc267997c06ca9ee8cc34
rdowd <rjarrettdowd@yahoo.com>
2020-07-08 21:03:03 -0400

    few small edits.

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -47,8 +47,8 @@ buckets.
 
 What is the time and space complexity of the counting sort algorithm?
 '''
-def counting_sort(arr, maximum=None):
-    # Your code here
+# def counting_sort(arr, maximum=None):
+     # Your code here
 
 
-    return arr
+#     return arr
