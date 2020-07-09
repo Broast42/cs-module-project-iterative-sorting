e205c88f618c0e871eb836da47c4143b94bc1f8a
rdowd <rjarrettdowd@yahoo.com>
2020-07-08 21:58:03 -0400

    Completed liniar and binary search

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,6 +1,8 @@
 def linear_search(arr, target):
     # Your code here
-
+    for i in range(len(arr)):
+        if arr[i] == target:
+            return i
 
     return -1   # not found
 
@@ -9,6 +11,19 @@ def linear_search(arr, target):
 def binary_search(arr, target):
 
     # Your code here
+    start = 0
+    end = (len(arr) - 1)
+    found = False
+    result = -1
 
-
-    return -1  # not found
+    while end >= start and not found:
+        mid = (start + end) // 2
+        if arr[mid] == target:
+            found = True
+            result = mid
+        else:
+            if target < arr[mid]:
+                end = mid - 1
+            if target > arr[mid]:
+                start = mid + 1
+    return result  # not found
